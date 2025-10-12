e93d33fd016c43ffb78bd4662a8c4d9ff7cc366e
Eduard <eduard.c.stoica10@gmail.com>
2025-10-12 21:36:17 +0300

    chore: use Batch()

--- src/recombee.ts
+++ src/recombee.ts
@@ -5,17 +5,23 @@ import fs from "fs";
 
 export const resetCatalog = async (recombeeClient: ApiClient) => {
   try {
-    // Get all items
-    const itemsResponse = await recombeeClient.send(new requests.ListItems({
+    const itemsResponse = await recombeeClient.send(
+      new requests.ListItems({
         returnProperties: true,
-    }));
+      })
+    );
 
-    // Delete each item
-    for (const item of itemsResponse) {
-      await recombeeClient.send(new requests.DeleteItem(item.itemId));
-      console.log(`Deleted item: ${item.itemId}`);
+    const deletedMovies = itemsResponse.map(
+      (item) => new requests.DeleteItem(item.itemId)
+    );
+
+    if (!itemsResponse?.length) {
+      console.log("No items found — catalog already empty.");
+      return;
     }
-    console.log("Catalog reset complete.");
+
+    await recombeeClient.send(new requests.Batch(deletedMovies));
+    console.log("🎉 Catalog reset complete.");
   } catch (err) {
     console.warn("Error resetting catalog:", err);
   }
@@ -23,35 +29,38 @@ export const resetCatalog = async (recombeeClient: ApiClient) => {
 
 export const uploadMovies = async (recombeeClient: ApiClient) => {
   const parser = fs
-    .createReadStream("/Users/edstoica/lab-sr/dist/IMBD.csv")
+    .createReadStream("/Users/edstoica/lab-sr/src/IMBD.csv")
     .pipe(parse({ columns: true, skip_empty_lines: true }));
 
+  const movies = new Array();
   for await (const record of parser) {
     const movieId = randomUUID();
-    try {
-      await recombeeClient.send(
-        new requests.SetItemValues(
-          movieId,
-          {
-            title: record.title,
-            description: record.description,
-            rating: Number(record.rating),
-            votes: Number(record.votes),
-            duration: Number(record.duration),
-            stars: record.stars
-              .split(", ")
-              .map((s: string) => s.trim())
-              .filter((s: string) => s !== "|" && s.toLowerCase() !== "stars:"),
-            genre: record.genre.split(", ").map((g: string) => g.trim()),
-          },
-          {
-            cascadeCreate: true,
-          }
-        )
-      );
-      console.log(`${movieId} added.`);
-    } catch (err) {
-      console.warn(err);
-    }
+    movies.push(
+      new requests.SetItemValues(
+        movieId,
+        {
+          title: record.title,
+          description: record.description,
+          rating: Number(record.rating),
+          votes: Number(record.votes),
+          duration: Number(record.duration),
+          stars: record.stars
+            .split(", ")
+            .map((s: string) => s.trim())
+            .filter((s: string) => s !== "|" && s.toLowerCase() !== "stars:"),
+          genre: record.genre.split(", ").map((g: string) => g.trim()),
+        },
+        {
+          cascadeCreate: true,
+        }
+      )
+    );
+  }
+
+  try {
+    await recombeeClient.send(new requests.Batch(movies));
+    console.log("🎉 Movies upload complete.");
+  } catch (err) {
+    console.warn(`Error uploading movies`, err);
   }
 };
